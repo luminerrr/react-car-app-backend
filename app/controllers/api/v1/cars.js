0edb4c56b32498bd839d176d9d4f55ef90dc48d6
Kevin <84540535+luminerrr@users.noreply.github.com>
2022-08-06 03:45:51 +0700

    created endpoint for get all cars data

--- app/controllers/api/v1/cars.js
+++ app/controllers/api/v1/cars.js
@@ -15,9 +15,27 @@ module.exports = {
                 error : err.message
             });
         });
-    };
+    },
 
+    addCar(req,res){
+        const carName = req.body.carName;
+        const price = req.body.price;
+        const model = req.body.model;
 
-    
+        Cars.create({
+            carName,
+            price,
+            model
+        })
+        .then((car)=>{
+            console.log('car successfully created')
+        })
+        .catch((err)=>{
+            res.status(401).json({
+                status:'error creating car',
+                error : err.message
+            })
+        })
+    }
 
 }
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const controllers = require('../app/controllers');
 
-router.get('/api/v1/cars');
+router.get('/api/v1/cars/getcars', controllers.api.v1.cars.getAllCars);
+
+module.exports = router;
