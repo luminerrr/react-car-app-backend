c77da0abaa5bac92613fc9d49da78c91868b07a9
Kevin <tkevinrocket@gmail.com>
2022-08-08 19:14:22 +0700

    added cors

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require('../config/routes');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors({origin: '*'}));
 app.use(router);
+
 
 module.exports = app
